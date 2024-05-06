c1f5290d40b0fd78a6f4e2f6ba4a84a56b61df55
ChetanGhodekar <chetanghodekar7875@gmail.com>
2024-05-06 16:44:27 +0530

    hnhnhnthnhy

--- DemoApplication2/src/main/java/Base/BaseClassTest.java
+++ DemoApplication2/src/main/java/Base/BaseClassTest.java
@@ -1,20 +1,10 @@
 package Base;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 
 
 public class BaseClassTest {
-	@Test
-	public void launchApplication() {
-		String bName="";
-		WebDriver webDriver;
-		if(bName.equals("Chrome")) {
-			System.out.println("CHROME");
-		}else {
-			System.out.println("DEFAULT BROWSER");
-		}
-	}
+	
 	
 	@Test
 	public void test() {
